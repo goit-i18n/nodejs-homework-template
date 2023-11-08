0d74cf1297e3360b32483444aa3f91049dd18df7
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-11-08 13:00:58 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const URL_DB = process.env.DB_URL;
+console.log(URL_DB);
+
+mongoose
+.connect(URL_DB)
+.then(() =>{
+  console.log("Serverul MongoDB ruleaza.");
+  app.listen(PORT, () => {
+    console.log(`Server is running. Use our API on port: ${PORT}`);
+  });
+})
+.catch((error)=> {
+  console.log(`Serverul nu ruleaza. Eroare: ${error.message}`)
+})
+
+
