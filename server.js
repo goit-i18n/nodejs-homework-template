624abcb21b126d93b69468bdd8b898459c7af8c5
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-11-11 17:17:57 +0200

    Upload contacts

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+const PORT = process.env.PORT || 5000;
+const urlDb = process.env.DB_HOST;
+
+mongoose.set("strictQuery", true);
+
+const connection = mongoose.connect(urlDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+connection
+  .then(() => {
+    console.log("\nDatabase connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("\nDatabase not running\n", err.toString());
+    process.exit(1);
+  });
+
+function signalHandler() {
+  mongoose.disconnect();
+  console.log("\nDatabase disconnected\n");
+}
+process.on("SIGINT", signalHandler);
