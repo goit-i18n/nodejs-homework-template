4d2c5754e1ac253f27b24e403ac64a9c37951ea0
AlexStan141 <61749830+AlexStan141@users.noreply.github.com>
2023-11-18 10:08:22 +0200

    another commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    res.json({ message: "test hw2" });
+    res.json({ message: "test" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
