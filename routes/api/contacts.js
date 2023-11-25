8c942ff4cf6c7f8b931d6b6d41d220041a5b623a
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-11-25 10:12:33 +0200

    Upload files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,91 @@
-const express = require('express')
+import express from "express";
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+  updateContact,
+} from "../../models/contacts.js";
 
-const router = express.Router()
+const contactsRouter = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/", async (req, res, next) => {
+  res.json({
+    status: "succes",
+    code: 200,
+    data: await listContacts(),
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!(contact instanceof Error))
+    res.status(200).json({
+      status: "succes",
+      code: 200,
+      data: contact,
+    });
+  else
+    res.status(contact.cause).json({
+      status: contact.name,
+      code: contact.cause,
+      message: contact.message,
+    });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.post("/", async (req, res, next) => {
+  const respondAddContact = await addContact(req.body);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!(respondAddContact instanceof Error))
+    res.status(201).json({
+      status: "succes",
+      code: 201,
+      data: respondAddContact,
+    });
+  else
+    res.status(respondAddContact.cause).json({
+      status: respondAddContact.name,
+      code: respondAddContact.cause,
+      message: respondAddContact.message,
+    });
+});
 
-module.exports = router
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const deleteContact = await removeContact(id);
+
+  if (!(deleteContact instanceof Error))
+    res.status(200).json({
+      status: "succes",
+      code: 200,
+      message: deleteContact.message,
+    });
+  else
+    res.status(deleteContact.cause).json({
+      status: deleteContact.name,
+      code: deleteContact.cause,
+      message: deleteContact.message,
+    });
+});
+
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const respondPutContact = await updateContact(id, req.body);
+
+  if (!(respondPutContact instanceof Error))
+    res.status(200).json({
+      status: "succes",
+      code: 200,
+      data: respondPutContact,
+    });
+  else
+    res.status(respondPutContact.cause).json({
+      status: respondPutContact.name,
+      code: respondPutContact.cause,
+      message: respondPutContact.message,
+    });
+});
+
+export default contactsRouter;
