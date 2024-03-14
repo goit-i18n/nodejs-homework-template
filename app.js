0bb7d04166277965567c70f50357dc7ed84268d7
Florin <florinfire80@yahoo.com>
2024-03-14 15:11:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,11 @@ const indexRouter = require("./routes/index");
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/users");
 
-
-
-
-
 const { connectToDb } = require("./db");
 const cors = require("cors");
 const app = express();
 
 connectToDb();
-
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -27,40 +22,23 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use("/", indexRouter);
 app.use("/api/contacts", contactsRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  console.log("Received request for:", req.url);
   next(createError(404, "Not Found"));
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-
- 
-
-
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-
   res.status(err.status || 500).json({ error: err.message });
-
 });
 
 module.exports = app;
