7dbca34bdf02730c8645728e027a2c02d34f3536
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-06-01 15:43:52 +0300

    commit stashed changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+<<<<<<< Updated upstream
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+=======
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import contactsRouter from "./routes/api/contacts.js";
+>>>>>>> Stashed changes
 
 const app = express()
 
