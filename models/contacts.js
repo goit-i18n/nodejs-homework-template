00fa232baffae4dcf42bc5da85678d84effd060d
OxS90 <oxana.slivinschi@gmail.com>
2024-06-17 05:50:25 +0300

    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,47 @@
-// const fs = require('fs/promises')
+import contacts from "./contacts.json" assert { type: "json" };
+import { v4 as uuidv4 } from "uuid";
 
-const listContacts = async () => {}
+const contactsService = {
+	listContacts,
+	getById,
+	removeContact,
+	addContact,
+	updateContact,
+};
 
-const getContactById = async (contactId) => {}
-
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+async function listContacts() {
+	return contacts;
 }
+
+async function getById(contactId) {
+	return contacts.find((el) => el.id === contactId);
+}
+
+async function removeContact(contactId) {
+	const index = contacts.findIndex((contact) => contact.id === contactId);
+	if (index !== -1) {
+		contacts.splice(index, 1);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+async function addContact(contact) {
+	const preparedContact = {
+		id: uuidv4(),
+		...contact,
+	};
+	contacts.push(preparedContact);
+}
+async function updateContact(contactId, body) {
+	const index = contacts.findIndex((contact) => contact.id === contactId);
+	if (index !== -1) {
+		contacts[index] = { ...contacts[index], ...body };
+		return contacts[index];
+	} else {
+		return null;
+	}
+}
+
+export default contactsService;
