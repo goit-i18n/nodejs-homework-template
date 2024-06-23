0b979e6272e2a1cd60c8a32d04155b1a8e793702
onisimsuldac <onnisimsuldac@gmail.com>
2024-06-23 13:05:58 +0300

    test home

---
+++ utils/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+async function dbConnection() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://onnisimsuldac:jRtJ9WNBhn02kUtW@cluster0.mrqqycy.mongodb.net/db-contacts"
+    );
+    console.log(colors.bgGreen.italic.bold("Database connection successful!"));
+  } catch (error) {
+    console.error(colors.bgRed.italic.bold(error));
+    process.exit(1);
+  }
+}
+
+module.exports = dbConnection;
