29c5103b731151e78f2344ec4f7824c0d31e119f
exigibil <adrian.topa04@gmail.com>
2024-07-26 22:08:08 +0300

    hw mongoose

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
+const { default: mongoose } = require("mongoose");
 
-const app = express()
+const connectionString =
+  "mongodb+srv://adriantopa04:adiadi@goit0.t06usgd.mongodb.net/?retryWrites=true&w=majority&appName=Goit0";
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+  mongoose
+  .connect(connectionString, {
+    dbName: "db-contacts",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  });
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use('/api/contacts', contactsRouter)
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,77 +1,45 @@
-const express = require('express');
-const path = require("path");
-const fs = require("fs").promises;
-const router = express.Router()
-const Joi = require('joi');
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const Contact = require("../../models/ContactsSchema");
+const mongoose = require("mongoose");
 
 const postSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required()
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 const putSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
-  phone: Joi.string()
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
 });
 
-const dataFile = path.join(__dirname, "../../models", "contacts.json");
 
-const readData = () => 
-  new Promise((resolve, reject) => {
-    fs.readFile(dataFile, "utf-8")
-      .then(data => resolve(JSON.parse(data)))
-      .catch(err => reject(err));
-  });
-
-
-const writeData = (data) => 
-  new Promise((resolve, reject) => {
-    fs.writeFile(dataFile, JSON.stringify(data, null, 4), "utf-8")
-      .then(() => resolve())
-      .catch(err => reject(err));
-  });
-  
-  const updateContact = async (contactId, updateData) => {
-    const data = await readData();
-    const index = data.findIndex(c => c.id === contactId);
-
-    if (index !== -1) {
-      if (updateData.name) {
-        data[index].name = updateData.name;
-      }
-      if (updateData.email) {
-        data[index].email = updateData.email;
-      }
-      if (updateData.phone) {
-        data[index].phone = updateData.phone;
-      }
-
-      await writeData(data);
-      return data[index]; 
-    } else {
-      return null; 
-    }
-  };
-  
-
-router.get('/', async (req, res, next) => {
-try {
-  const data = await readData();
-  res.status(200).json(data);
-
-} catch (err) {
-  console.error(err)
-  res.status(500).json( {error: "Internal Server Error" } );
-}
- 
-})
-
-router.get('/:contactId', async (req, res, next) => {
+// Route GET all contacts
+router.get("/", async (req, res, next) => {
   try {
-    const data = await readData();
-    const contact = data.find(c => c.id === req.params.contactId);
+    const data = await Contact.find();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Route GET by ID
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const contactId = req.params.contactId;
+
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(400).json({ error: "ID contact invalid" });
+    }
+
+    const contact = await Contact.findById(contactId);
     if (contact) {
       res.status(200).json(contact);
     } else {
@@ -83,23 +51,15 @@ router.get('/:contactId', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res) => {
+// Route POST
+router.post("/", async (req, res) => {
   try {
-
     const { error } = postSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
-
-    const data = await readData();
-    const newItem = {
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone
-    };
-    data.push(newItem);
-    await writeData(data);
+    const newItem = new Contact(req.body);
+    await newItem.save();
     res.status(201).json(newItem);
   } catch (err) {
     console.error(err);
@@ -107,41 +67,106 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:contactId', async (req, res, next) => {
+// Route DELETE
+router.delete("/:contactId", async (req, res) => {
   try {
-    const data = await readData();
-    const index = data.findIndex(c => c.id === req.params.contactId);
-    if (index !== -1) {
-      data.splice(index, 1);
-      await writeData(data)
-      res.status(200).json({ message: 'Contact deleted successfully' });
+    const contactId = req.params.contactId;
+
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(400).json({ error: "invalid contactId" });
+    }
+
+    const result = await Contact.deleteOne({ _id: contactId });
+
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: "Contact deleted successfully" });
     } else {
       res.status(404).json({ error: "Contact not found" });
     }
   } catch (err) {
-    console.error(err);
+    console.error("Delete contact error :", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
-})
+});
 
-router.put('/:contactId', async (req, res) => {
+// Route PUT
+router.put("/:contactId", async (req, res) => {
   try {
     const { error } = putSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
 
-    const { name, email, phone } = req.body;
+    const { name, email, phone, favorite } = req.body;
 
-    if (!name && !email && !phone) {
-      return res.status(400).json({ message: 'missing fields' });
+    if (!name && !email && !phone && favorite === undefined) {
+      return res.status(400).json({ message: "missing fields" });
     }
 
-    const updatedContact = await updateContact(req.params.contactId, {
-      name,
-      email,
-      phone
-    });
+    const contactId = req.params.contactId;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(400).json({ error: "Invalid contact ID" });
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      {
+        name,
+        email,
+        phone,
+        favorite,
+      },
+      { new: true }
+    );
+
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+const updateStatusContact = async (contactId, body) => {
+  try {
+ 
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return null;
+    }
+
+   
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId, 
+      { favorite: body.favorite }, 
+      { new: true } 
+    );
+
+    return updatedContact;
+  } catch (err) {
+    console.error('Error updating contact status:', err);
+    return null;
+  }
+};
+
+
+router.patch('/:contactId/favorite', async (req, res) => {
+  try {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+
+    if (favorite === undefined) {
+      return res.status(400).json({ message: 'missing field favorite' });
+    }
+
+    if (typeof favorite !== 'boolean') {
+      return res.status(400).json({ message: 'field favorite must be a boolean' });
+    }
+
+    const updatedContact = await updateStatusContact(contactId, { favorite });
 
     if (updatedContact) {
       res.status(200).json(updatedContact);
@@ -149,9 +174,9 @@ router.put('/:contactId', async (req, res) => {
       res.status(404).json({ message: 'Not found' });
     }
   } catch (err) {
-    console.error(err);
+    console.error('Error handling PATCH request:', err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-module.exports = router
+module.exports = router;
