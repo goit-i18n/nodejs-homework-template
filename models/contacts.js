ea9f5e57f656bba029339e9ec31fba268fcf4ac8
czke33 <czke33@gmail.com>
2024-07-30 20:36:02 +0300

    fist commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,18 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+const {nanoid} = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+ const data=await fs.readFile(contactsPath);
+ return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const getById = async (id) => {
+  const contactId = String(id);
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;  
+}
+  
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (id) => { 
+  const contactId = String(id);
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts.splice(index, 1); 
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return "Contact deleted successfully";
+}
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data
+  }
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact; 
+}
+
+const updateContact = async (contactId, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id: contactId, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+}
+
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
