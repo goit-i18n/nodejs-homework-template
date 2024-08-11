d51e0b39494707d7ab5e71b8c95ed8c4ac87d160
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-08-12 00:26:09 +0100

    Finish Homework 02

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,91 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
+require("colors");
 
-const listContacts = async () => {}
+const pathContacts = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const readContacts = async()=> {
+  try{
+    const data = await fs.readFile(pathContacts, 'utf-8');
+    console.log('Contacts successfully read!');
+    return JSON.parse(data);
+  }catch(err){
+    console.error('Error reading contacts:', err.message);
+    return [];
+  }
+}
 
-const removeContact = async (contactId) => {}
+const writeContactsFile = async (contacts) => {
+  try {
+    await fs.writeFile(pathContacts, JSON.stringify(contacts, null, 2));
+    console.log('Contacts successfully written to file!'.green);
+  } catch (err) {
+    console.error('Error writing contacts to file!', err.red);
+  }
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const contacts = await readContacts();
+  console.log('Contacts successfully returned!'.blue);
+  console.table(contacts);
+  return contacts;
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContacts();
+  const contactById = contacts.find(contact => contact.id === contactId);
+  console.log(`Contact with ID ${contactId} is displayed!`.blue);
+  return contactById;
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await readContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if(index > -1){
+    const removedContact = contacts.splice(index, 1);
+    await writeContactsFile(contacts);
+    console.log(`Contact with ID ${contactId} is removed!`.blue);
+    console.table(contacts);
+    return removedContact[0];
+  }
+  console.log('Contact not found for remove!'.red);
+  return null;
+}
+
+
+const addContact = async (body) => {
+  console.log('Body received:', body);
+  const contacts = await readContacts();
+
+  const newContact = {
+    id: nanoid(),
+    ...body
+  };
+  contacts.push(newContact);
+  console.log('Contacts before writing:', contacts);
+  await writeContactsFile(contacts);
+  console.log(`Contact with ID ${newContact.id} added successfully!`.blue);
+  console.table(contacts);
+  return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = await readContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if (index > -1) {
+    const updatedContact = {
+      ...contacts[index],
+      ...body
+    };
+    contacts[index] = updatedContact;
+    await writeContactsFile(contacts);
+    console.log(`Contact with ID ${contactId} updated!`.blue);
+    return updatedContact;
+  }
+  console.log('Contact not found for update!'.red);
+  return null;
+};
 
 module.exports = {
   listContacts,
