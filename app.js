7b0f35a431ae7d489edf890cae0a36d45b190763
Andree-Ioana <ioana.vinereanu@stud.etti.upb.ro>
2024-08-17 16:57:07 +0300

    avatar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -13,6 +14,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
