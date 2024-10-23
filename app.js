3a6a6cd0ba22ed95957d0e93d3c08bde1e4d9d37
Madalina Gomei <madalinagomei@gmail.com>
2024-10-23 21:04:01 +0200

    add

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import contactsRouter from "./routes/api/contacts.js";
 import usersRouter from "./routes/api/users.js";
 import auth from "./middlewares/auth.middleware.js";
 
+// Use __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+//  fisiere statice din folderul public
+app.use("/avatars", express.static(path.join(__dirname, "public/avatars")));
 
 app.use(logger(formatsLogger));
 app.use(cors());
