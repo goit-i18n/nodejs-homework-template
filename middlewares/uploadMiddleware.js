9f7535ccdc31fc01213214a4d3a9d2b4fd3d659a
SecaraAdrian <secaforce@gmail.com>
2024-11-04 22:55:07 +0200

    resolve conflicts

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,21 +1,14 @@
 const multer = require('multer');
 const path = require('path');
 
-<<<<<<< Updated upstream
-const tempDir = path.join(__dirname, '../tmp');
-const storage = multer.diskStorage({
-    destination: tempDir,
-    filename: (req, file, cb) => cb(null, `${req.user._id}-${Date.now()}${path.extname(file.originalname)}`),
-=======
 const tmpDir = path.join(__dirname, '../tmp');
 
 const storage = multer.diskStorage({
-  destination: tmpDir,
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
->>>>>>> Stashed changes
+    destination: tmpDir,
+    filename: (req, file, cb) => {
+        const uniqueSuffix = `${req.user._id}-${Date.now()}${path.extname(file.originalname)}`;
+        cb(null, uniqueSuffix);
+    },
 });
 
 const upload = multer({ storage });
