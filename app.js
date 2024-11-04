78839edf151c7cfc1ffa53527ad846b56ce4b93a
SecaraAdrian <secaforce@gmail.com>
2024-11-04 22:56:31 +0200

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Asigură-te că ai public pentru a servi fișiere statice
+app.use(express.static('public')); 
 
 app.use('/avatars', express.static('public/avatars'));
 app.use('/api/auth', authRouter);
